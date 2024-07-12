1e57d1218dc0d3f0c4a6b684e62dbf6a5fcc170a
drishti1611 <146825084+drishti1611@users.noreply.github.com>
2024-07-12 16:02:00 +0530

    Exceptional Handling

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorPract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] number = { 1, 2, 3, 4, 5, 6, 7, };
+                Console.WriteLine(number[9]); //System.IndexOutOfRangeException: 'Index was outside the bounds of the array.'
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error Occured: Index is out of range ");
+            }
+
+            finally
+            {
+                Console.WriteLine("Execution Successful!");
+            }
+        }
+    }
+}
